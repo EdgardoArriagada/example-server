92cf0181aa15d2618a6fc7bf07b78460b71bbf28
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-03-29 15:19:53 -0300

    before modularizing

--- src/main.rs
+++ src/main.rs
@@ -18,3 +18,21 @@ impl Server {
     println!("Listening on {}", self.addr);
   }
 }
+
+struct Request {
+  path: String,
+  query_string: Option<String>,
+  method: Method,
+}
+
+enum Method {
+  GET,
+  DELETE,
+  POST,
+  PUT,
+  HEAD,
+  CONNECT,
+  OPTIONS,
+  TRACE,
+  PATCH
+}
