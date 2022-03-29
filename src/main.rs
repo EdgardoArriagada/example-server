0a2017be83e0542c0aa08ff9a81bfead5b84d80b
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-03-29 14:34:19 -0300

    wip server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
 fn main() {
-    println!("Hello, world!");
+  let server = Server::new("127.0.0.1:8080".to_string());
+  server.run()
+}
+
+struct Server {
+  addr: String,
+}
+
+impl Server {
+  fn new(addr: String) -> Self {
+    Self {
+      addr
+    }
+  }
+
+  fn run(self) {
+    println!("Listening on {}", self.addr);
+  }
 }
