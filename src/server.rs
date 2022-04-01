45a1c8f955de5048fd1e4164caf5d2a9e3bfd6f9
EdgardoArriagada <edgardo.arriagada.ortiz@gmail.com>
2022-03-31 21:06:28 -0300

    parse key and value

--- src/server.rs
+++ src/server.rs
@@ -1,8 +1,28 @@
 use std::io::Read;
 use std::net::TcpListener;
 
+use std::str::FromStr;
+
 pub struct Server {
     addr: String,
+}
+
+struct KeyValue {
+    key: String,
+    value: String,
+}
+
+impl FromStr for KeyValue {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let key_val: Vec<&str> = s.split(' ').collect();
+        
+        Ok(KeyValue {
+            key: key_val[0].to_string(),
+            value: key_val[1].to_string(),
+        })
+    }
 }
 
 impl Server {
@@ -23,7 +43,10 @@ impl Server {
                     let mut buffer = [0; 1024];
                     match stream.read(&mut buffer) {
                         Ok(_) => {
-                            println!("Received a request: {}", String::from_utf8_lossy(&buffer));
+                            let data = &String::from_utf8_lossy(&buffer);
+                            let tuple  = KeyValue::from_str(data).unwrap();
+                            
+                            println!("key: {}, value: {}", tuple.key, tuple.value);
                         },
                         Err(e) => println!("Failed to establish a connection: {}", e),
                     }
